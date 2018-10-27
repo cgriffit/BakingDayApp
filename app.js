d792712c256b88bf9538ae1c709566d013785056
cgriffit <chanson1891@gmail.com>
2018-10-27 14:49:16 -0500

    add popup form

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
   //toggle display of list and associated styling
-  var foodCategories = document.querySelectorAll(".foodType");
+  let foodCategories = document.querySelectorAll(".foodType");
   foodCategories.forEach(function(category) {
     category.addEventListener("click", function() {
       this.nextElementSibling.classList.toggle("hide");
     });
+  });
+
+  //show form on button click
+  let addFood = document.getElementById("addFood");
+  let form = document.getElementById("form");
+  addFood.addEventListener("click", function() {
+    form.classList.toggle("hide");
+    form.classList.toggle("formContainer");
   });
 
 });
